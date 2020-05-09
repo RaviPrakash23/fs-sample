b22b746698a85abb5140337eeab153b580cb6ca7
yraviui <yravi.ui@gmail.com>
2020-05-08 17:36:57 -0700

    Initial Commit. Created Spring Boot and configure the application properties

---
+++ fssample/src/main/java/club/yravi/fssample/FsSampleApplication.java
@@ -0,0 +1,13 @@
+package club.yravi.fssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsSampleApplication.class, args);
+	}
+
+}
