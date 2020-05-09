7d1a0fe965594c02c29da0061bf3b0b6db5d50d2
yraviui <yravi.ui@gmail.com>
2020-05-09 10:25:57 -0700

    Completed Feature one.

--- fssample/src/main/java/club/yravi/fssample/repository/BookCategoryRepository.java
+++ fssample/src/main/java/club/yravi/fssample/repository/BookCategoryRepository.java
@@ -1,9 +1,11 @@
 package club.yravi.fssample.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import club.yravi.fssample.entity.BookCategory;
 
+@RepositoryRestResource(collectionResourceRel = "bookCateogry", path="book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 
 }
